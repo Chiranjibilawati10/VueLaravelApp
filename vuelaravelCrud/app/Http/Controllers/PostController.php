99d5aced99d5c219052b421bd43524f72aa6cfac
Chiranjibi Lawati <chiranjibilawati08@gmail.com>
2020-01-27 21:10:10 +0545

    does curd function but need to solve error in api

---
+++ vuelaravelCrud/app/Http/Controllers/PostController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resource\PostCollection;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function store(Request $request)
+    {
+        //storing data in  variable and creating record
+        $post = new Post([
+            'title' => $request->get('title'),
+            'body'  => $request->get('body')
+        ]);
+
+        $post->save();
+
+        return response()->json('success');
+    }
+
+    public function index()
+    {
+        return new PostCollection(Post::all());
+    }
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        
+        return response()->json($post);
+    }
+
+    public function update($id, Request $request)
+    {
+        $post = Post::find($id);
+        $post->update($request->all());
+
+        return response()->json('Data updated.');
+    }
+
+    public function delete($id)
+    {
+        $post = Post::find($id);
+
+        $post->delete();
+
+        return response()->json('Data deleted.');
+    }
+}
---
+++ vuelaravelCrud/resources/js/app.js
@@ -0,0 +1,63 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+
+Vue.use(VueAxios, axios);
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('app-component', require('./App.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+//routes path
+const routes = [
+    { path: '/', component: HomeComponent},
+    { path:'/create', component: CreateComponent},
+    { path:'/posts', component:IndexComponent},
+    { path:'/edit/:id', component:EditComponent}
+  ];
+  
+  // const router = new VueRouter({ mode: 'history', routes: routes});
+  // const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+  //creating new router and passing the routes
+const mode = 'history';
+const router = new VueRouter({routes, mode});
+
+const app = new Vue({
+    router,
+    el: '#app',
+});
+
+
+
